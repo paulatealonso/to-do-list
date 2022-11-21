7bbba327ea55aa6a3616581d8fd0c8104ba9dc90
User <paulate.alonso@gmail.com>
2022-11-21 15:26:03 +0100

    BtnRemAll

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const noteForm = document.querySelector("#note-form")
 const noteInput = document.querySelector("#input-note")
 const submitButton = document.querySelector("#submitButton")
 const notes = document.querySelector("#notes")
-
 const buttonRemoved = `<button class= "buttonRemove" onclick = "removeNote(this)"> <span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span>
 </button>`
-
+const removeAll = document.querySelector("#submitButton2")
 
 
 
@@ -34,3 +33,21 @@ const removeNote = (button) => {
 }
 
 
+ // TAMPOCO PERO ESTA BETTER//
+ 
+removeAll.onclick = () => {
+    let lis = notes.getElementsByTagName("li");
+    while(lis.length > 0){                   
+       notes.removeChild(lis[0]);
+    }
+    notesStorage = []
+    localStorage.setItem("notesStr", JSON.stringify(notesStorage))
+ };  
+ 
+
+
+ /*removeAll.onclick = () => {            
+    notesStorage = []
+    localStorage.setItem("notesStr", JSON.stringify(notesStorage))
+    notes.remove()
+ };   NO SALE */ 
